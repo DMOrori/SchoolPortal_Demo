b314e8b0449c6ce3f3a4613f69b668a82bd73f52
derrick-maebar <derrickorori@gmail.com>
2026-02-14 19:50:25 +0300

    updated the register activity

--- app/src/main/java/com/example/schoolportal/RegisterActivity.kt
+++ app/src/main/java/com/example/schoolportal/RegisterActivity.kt
@@ -1,6 +1,10 @@
 package com.example.schoolportal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,17 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val etName = findViewById<EditText>(R.id.etName)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        btnRegister.setOnClickListener {
+            Toast.makeText(this,"Registration successfull",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
         }
     }
 }
